b9ce7422cbaf74a66097bf68700e400ba9684a8e
AIDDEI <aidendeightonschool@gmail.com>
2022-09-29 15:56:43 +0200

    Basis HTML pagina layout

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="resources/css/app.css" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Document</title>
-</head>
-<body>
-<h1>{{$help}}</h1>
-<a href={{route('home')}}>terug</a>
-</body>
-</html>
+@extends('layouts.web')
+@section('title', 'help')
+@section('content')
+    <h1>{{$help}}</h1>
+    <a href={{route('home')}}>terug</a>
+@endsection
